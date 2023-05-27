3c83e850bc58ebe11a0e7cdb22dfca4b71dc2f11
Phaedra <71579580+ph4iry@users.noreply.github.com>
2023-05-27 12:31:16 -0400

    debugging remote process

--- ts/Client.ts
+++ ts/Client.ts
@@ -23,7 +23,7 @@ export default class Client {
   static #terms = Object.keys(Client.#markingPeriods);
 
   async init() {
-    this.browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: false });
+    this.browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: true });
     this.page = await this.browser.newPage();
 
     await this.page.goto('http://sis.mybps.org/aspen/logon.do');
